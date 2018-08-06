c338bdaa6d061b715ee5fa6f23c48ce04879ce0a
Matthieu <matthieunadeau96@gmail.com>
2018-08-06 11:50:32 -0400

    added random equation and answers

---
+++ src/components/MainScreen/Answer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Answer extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.answer}</p>
+      </div>
+    );
+  }
+
+}
+
+export default Answer;
--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+
 import Question from './Question';
+import Answer from './Answer';
 
 class MainScreen extends Component {
+
+  state = {
+    numberOne: undefined,
+    numberTwo: undefined,
+    sign: undefined,
+    realAnswer: undefined,
+    answer: undefined
+  }
+
+  componentDidMount() {
+    let numberOne = Math.floor(Math.random() * 20);
+    let numberTwo = Math.floor(Math.random() * 20);
+    let operators = [{ sign: "+", method: function(n1, n2){ return n1 + n2; } }, { sign: "-", method: function(n1, n2) { return n1 - n2; } }];
+    let selectedOperator = Math.floor(Math.random() * operators.length);
+
+    let sign = (operators[selectedOperator].sign);
+    let realAnswer = operators[selectedOperator].method(numberOne, numberTwo)
+
+    let answer = Math.floor(Math.random() * 20);
+
+
+    console.log("Real Answer: " + realAnswer);
+
+    this.setState({
+      numberOne,
+      numberTwo,
+      sign,
+      realAnswer,
+      answer
+    })
+  }
 
   render() {
     return (
       <div>
         <p>Timer Bar</p>
-        <Question/>
-        <p>Answer Equation</p>
+        <Question
+          numberOne={this.state.numberOne}
+          numberTwo={this.state.numberTwo}
+          sign={this.state.sign}/>
+        <Answer answer={this.state.answer}/>
         <p>No</p><p>Yes</p>
         <p>Score: 0</p>
       </div>
